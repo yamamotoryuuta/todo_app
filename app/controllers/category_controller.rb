b2526e9539ff643185fc9b7622788b424ce60d73
山本龍太 <yamamotoryuuta@yamamotoryuutanoiMac.local>
2020-06-13 08:24:30 +0900

    add_category_edit

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,5 @@
 class CategoryController < ApplicationController
+  before_action :set_category, only: %i(edit update)
   def new
     @category = Category.new
   end
@@ -13,17 +14,24 @@ class CategoryController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @category.update_attributes(params_category)
+      redirect_to :root
+    else
+      render action: :edit
+    end
   end
 
   private
 
   def params_category
     params.require(:category).permit(:title).merge(user: current_user)
+  end
+
+  def set_category
+    @category = Category.find_by(id: params[:id])
   end
 
 
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
     @category = Category.where(user: current_user)
-    @task = Task.all
+    @task = Task.where(user: current_user)
   end
 end
